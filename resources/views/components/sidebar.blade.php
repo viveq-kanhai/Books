c4979c81e56c7f9aba1f2e5796e38ca9f29d31f8
viveq-kanhai <viveqmisre@outlook.com>
2023-11-17 18:32:52 -0300

    Added sidebar

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,3 @@
+<div class="bg-white w-16 hover:w-48 duration-500">
+            sidebar
+        </div>
--- resources/views/models/subjects/index.blade.php
+++ resources/views/models/subjects/index.blade.php
@@ -5,52 +5,56 @@
 @section('content')
 
 <div class="flex flex-col w-full bg-yellow-500">
-    <div class="bg-neutral-800 h-screen w-full">
-        <x-navbar/>
-        <div class="mt-14 py-2 flex justify-end text-white">
-            <div class="flex flex-nowrap mt-2 mr-4 md:mr-8">
-                <form>
-                    <input
-                    class="block rounded-l border-2 h-8 border-white w-32 md:w-auto"
-                    name="search"
-                    type="text"
-                    placeholder="Search"
-                    />
-                </form>
-                <span class="text-md cursor-pointer bg-cyan-500 rounded-r h-8 border-2">
-                    <ion-icon name="search-sharp" class="px-2 text-xl cursor-pointer h-8 text-white"></ion-icon>
-                </span>
+    <div class="bg-neutral-800 h-screen w-full flex">
+        <x-sidebar/>
+        <div class="w-full">
+            <div class="mt-4 py-2 flex justify-end text-white">
+                <div class="flex flex-nowrap mt-2 mr-4 md:mr-8">
+                    <form>
+                        <input
+                        class="block rounded-l border-2 h-8 border-white w-32 md:w-auto"
+                        name="search"
+                        type="text"
+                        placeholder="Search"
+                        />
+                    </form>
+                    <span class="text-md cursor-pointer bg-cyan-500 rounded-r h-8 border-2">
+                        <ion-icon name="search-sharp" class="px-2 text-xl cursor-pointer h-8 text-white"></ion-icon>
+                    </span>
+                </div>
             </div>
-        </div>
-        <div class="mt-4 md:mt-8 flex">
-            <div class="w-[70%]">
-                <div class="bg-white h-12 mx-4 md:mx-8 mb-1 rounded shadow-lg cursor-pointer" id="product">
-                    <div class="flex justify-between" id="details">
-                        <div class="text-xl font-semibold p-2">
-                            Subject name
-                        </div>
-                        <div class="bg-cyan-500 w-12 h-12 rounded-r border-2">
-                        <ion-icon class="text-xl pt-3 pl-3" name="pencil"></ion-icon>
+            <div class="mt-4 md:mt-8 flex">
+                <div class="w-[70%]">
+                    <div class="bg-white h-12 mx-4 md:mx-8 mb-1 rounded shadow-lg cursor-pointer" id="product">
+                        <div class="flex justify-between" id="details">
+                            <div class="text-xl font-semibold p-2">
+                                Subject name
+                            </div>
+                            <div class="w-20 h-auto flex justify-center pt-1">
+                                <div class="bg-cyan-500 w-10 h-10 rounded-full flex justify-center">
+                                    <ion-icon class="text-xl mt-3" name="pencil"></ion-icon>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="w-[30%]">
-                <div class='flex items-center justify-center h-72 w-90 mr-4 md:mr-8 rounded bg-white'>
-                    <form>
-                        <div class='flex flex-col mb-1'>
-                            <h2 class='mb-12 text-center text-black uppercase text-xl font-[Poppins] font-semibold'>Add subject</h2>
-                            <input class='text-black bg-transparent focus:outline-none w-60 h-9 border-2 border-solid border-black
-                            rounded-2xl p-1 text-base font-[Poppins]' 
-                            placeholder='                   Subject name'/>
-                        </div>
-                        <div class='flex justify-center mt-2 mr-8 w-full'>
-                            <button class='bg-cyan-500 text-white font-[Poppins]  mt-4 py-2 px-6 w-full rounded-3xl
-                            hover:bg-green-400 duration-500' type='button'>
-                                Add
-                            </button>
-                        </div>
-                    </form>
+                <div class="w-[30%]">
+                    <div class='flex items-center justify-center h-72 w-90 mr-4 md:mr-8 rounded bg-white'>
+                        <form>
+                            <div class='flex flex-col mb-1'>
+                                <h2 class='mb-12 text-center text-black uppercase text-xl font-[Poppins] font-semibold'>Add subject</h2>
+                                <input class='text-black bg-transparent focus:outline-none w-60 h-9 border-2 border-solid border-black
+                                rounded-2xl p-1 text-base font-[Poppins]' 
+                                placeholder='                   Subject name'/>
+                            </div>
+                            <div class='flex justify-center mt-2 mr-8 w-full'>
+                                <button class='bg-cyan-500 text-white font-[Poppins]  mt-4 py-2 px-6 w-full rounded-3xl
+                                hover:bg-green-400 duration-500' type='button'>
+                                    Add
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
