86d19a98e859c996f63d70b8936b93533314429e
MMarbs <soedamahchamil@gmail.com>
2023-12-05 20:57:08 -0300

    file no longer shareable hopefully

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,9 @@
             </div>
         </div>
     </div>
-    <iframe src='https://drive.google.com/file/d/1D1C5qxGsfrM8my9ZBexpWxa-ELxwC0ON/preview' style="border: none;">
+    <iframe src='https://drive.google.com/file/d/1D1C5qxGsfrM8my9ZBexpWxa-ELxwC0ON/preview'
+        style="border: none;"
+        sandbox="allow-scripts allow-same-origin">
     </iframe>
 </div>
 @endsection
